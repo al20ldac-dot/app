d477968c8baa5255d3afcabeab0a8fe40db7328d
Leonardo David Alvarado Cornejo <al20ldac@gmail.com>
2026-03-28 17:45:34 -0500

    Use NEXT_PUBLIC_FIREBASE_* env vars for Firebase config

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyB8FNWEXu4TUmdddTnJtq0l-8CpORXweaw",
-  authDomain: "studio-9295293021-d9724.firebaseapp.com",
-  projectId: "studio-9295293021-d9724",
-  storageBucket: "studio-9295293021-d9724.firebasestorage.app",
-  messagingSenderId: "179515490228",
-  appId: "1:179515490228:web:0f44ce6a0a40f0f678bce1"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyB8FNWEXu4TUmdddTnJtq0l-8CpORXweaw",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "studio-9295293021-d9724.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "studio-9295293021-d9724",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "studio-9295293021-d9724.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "179515490228",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:179515490228:web:0f44ce6a0a40f0f678bce1"
 };
